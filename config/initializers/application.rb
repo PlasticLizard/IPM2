59c20ecbb1edf49248117c76f4c33d3366b82720
unknown <Nathan@.(none)>
2010-05-21 22:26:42 -0700

    Changed database.yml to mongo.yml in the hopes of getting heroku to return my love.

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,5 +1,5 @@
 def get_database_config
-  YAML.load_file(Rails.root.join('config', 'database.yml'))
+  YAML.load_file(Rails.root.join('config', 'mongo.yml'))
 end
 
 MongoMapper.setup(get_database_config, Rails.env, {
